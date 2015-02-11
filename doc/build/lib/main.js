cad41893bba2fc2b20fbb354a96585e76f9de8ab
kissyteam <tiehang.lth@taobao.com>
2015-02-11 18:36:27 +0800

    sync doc automatically by kpm

--- doc/build/lib/main.js
+++ doc/build/lib/main.js
@@ -1,4 +1,4 @@
-KISSY.add('kg/paginations/1.0.0/lib/main',["node","base"],function(S ,require, exports, module) {
+KISSY.add('kg/paginations/1.0.1/lib/main',["node","base"],function(S ,require, exports, module) {
  
 var EMPTY = '';
 var $ = require('node').all;
